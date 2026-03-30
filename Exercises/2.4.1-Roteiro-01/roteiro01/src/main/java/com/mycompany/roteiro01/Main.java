c8ba1f73d4bbfd0d211f765802de85a1b7b1d6e1
Guilherme Bernardino Reis <guilhermeb.reis112@gmail.com>
2026-03-30 09:34:48 -0300

    create a new initial section ofr exercise

---
+++ Exercises/2.4.1-Roteiro-01/roteiro01/src/main/java/com/mycompany/roteiro01/Main.java
@@ -0,0 +1,16 @@
+// Package
+package com.mycompany.roteiro01;
+
+// Classes
+import classes.Triangulo;
+import classes.Quadrado;
+import classes.Ponto;
+
+// Utils
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
