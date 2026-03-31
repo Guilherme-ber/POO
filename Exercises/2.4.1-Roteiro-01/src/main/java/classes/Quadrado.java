0fcba92e19a86848a431eceac5eed1de7ce5bf8d
Guilherme Bernardino Reis <guilhermeb.reis112@gmail.com>
2026-03-31 16:21:07 -0300

    fetch: finalizing 'roteiro 1' exercise

--- Exercises/2.4.1-Roteiro-01/src/main/java/classes/Quadrado.java
+++ Exercises/2.4.1-Roteiro-01/src/main/java/classes/Quadrado.java
@@ -2,7 +2,7 @@ package classes;
 
 public class Quadrado {
     private Ponto p1, p2, p3, p4;
-
+    
     public Quadrado() {
     }
     public Quadrado(Ponto p1, Ponto p2, Ponto p3, Ponto p4) {
@@ -14,37 +14,39 @@ public class Quadrado {
 
     @Override
     public String toString() {
-        double area = lado * lado;
-        double perim = 4 * lado;
-        return "Quadrado [Lado: " + lado + "]" +
-               "\nArea: " + area + 
-               "\nPerimetro: " + perim;
+        return "Quadrado [Lado: " + this.p1.distance(this.p2) + "]" +
+               "\nArea: " + squareArea() + 
+               "\nPerimetro: " + perimeter() + 
+               "\nTipo: " + type();
     }
     
     public double squareArea() {
-        double height = getHeight();
-        double base = getBase();
-        return base * height;
+        // Base p1-p4 * Altura p1-p2
+        return (this.p1.distance(this.p2) * (this.p1.distance(this.p4)));
     }
     
     public double perimeter() {
         double ab = this.p1.distance(this.p2);
         double bc = this.p2.distance(this.p3);
         double cd = this.p3.distance(this.p4);
-        double ca = this.p4.distance(this.p1);
+        double da = this.p4.distance(this.p1);
         
-        return ab + bc + cd + ca;
+        return ab + bc + cd + da;
     }
     
-    public String type(Ponto p1, Ponto p2, Ponto p3) {
-        double a = p1.distance(p2);
-        double b = p2.distance(p3);
-        double c = p3.distance(p1);
+    public String type() {
+        // Deve ser digitado em ordem!!
+        double a = this.p1.distance(this.p2); // Lado 1
+        double b = this.p2.distance(this.p3); // Lado 2
+        double c = this.p3.distance(this.p4); // Lado 3
+        double d = this.p4.distance(this.p1); // Lado 4
         
-        if(a == b && b == c && c == a) return "Equilatero";
-        else if(a == b || b == c || c == a) return "Isosceles";
-        else if(a != b && b != c && c != a) return "Escaleno";
-        else return "Sem classificação";
+        boolean sameFaces = a == b && b == c && c == d && d == a;
+        double hypot = this.p1.distance(this.p3);
+        double hypotCalculate = Math.sqrt((d * d) + (c * c));
+        
+        if(sameFaces && hypot == hypotCalculate) return "Quadrado";
+        else return "Não é um quadrado perfeito ou não é um quadrado.";
     }
 
     public Ponto getP1() {
@@ -56,6 +58,9 @@ public class Quadrado {
     public Ponto getP3() {
         return p3;
     }
+    public Ponto getP4() {
+        return p4;
+    }
 
     public void setP1(Ponto p1) {
         this.p1 = p1;
@@ -65,5 +70,8 @@ public class Quadrado {
     }
     public void setP3(Ponto p3) {
         this.p3 = p3;
+    }  
+    public void setP4(Ponto p4) {
+        this.p4 = p4;
     }  
 }
--- Exercises/2.4.1-Roteiro-01/src/main/java/com/mycompany/roteiro01/Main.java
+++ Exercises/2.4.1-Roteiro-01/src/main/java/com/mycompany/roteiro01/Main.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Triângulo
         Ponto p1 = new Ponto(2, 2);
         Ponto p2 = new Ponto(4, 4);
         Ponto p3 = new Ponto(6, 2);
@@ -22,12 +23,22 @@ public class Main {
         double perimeter = t1.perimeter(p1, p2, p3);
         String type = t1.type(p1, p2, p3);
         
+        System.out.println("----- Triangulo -----");
         if(isColinear) System.out.println("O triangulo e colinear.");
         else System.out.println("O triangulo nao e colinear.");
-        
         System.out.println("Area do triangulo: " + triangleArea + "cm²");
         System.out.println("Perimetro do triangulo: " + perimeter + "cm");
+        System.out.println("Tipo do triangulo: " + type + "\n");
         
-        System.out.println("Tipo do triangulo: " + type);
+        // Quadrado
+        Ponto p4 = new Ponto(0, 0);
+        Ponto p5 = new Ponto(0, 2);
+        Ponto p6 = new Ponto(2, 2);
+        Ponto p7 = new Ponto(2, 0);
+        
+        Quadrado q1 = new Quadrado(p4, p5, p6, p7);
+        
+        System.out.println("----- Quadrado -----");
+        System.out.println(q1);
     }
 }
